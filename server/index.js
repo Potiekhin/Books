533102fcc8726d44674e424c50f736a8a6e52603
Oleh Potiekhin <oleh.potiekhin@dwreteam.com>
2021-07-20 14:34:17 +0300

    add delete method but not check it

--- server/index.js
+++ server/index.js
@@ -1,39 +1,59 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require("express")
+const bodyParser = require("body-parser")
 const path = require('path')
 const fs = require("fs")
 const cors = require("cors")
 
-const app = express();
+const app = express()
+const PORT = 8888
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(cors())
 
-const dbPath = path.join(__dirname,'db.json') 
+const dbPath = path.join(__dirname, 'db.json')
+
+app.get("/", (req, res) => {
+    fs.readFile(dbPath, (err, json) => {
+        let obj = JSON.parse(json)
+        res.json(obj)
+    });
+});
 
 app.post("/", (req, res) => {
     if (!req.body) {
-        return res.sendStatus(400);
+        return res.sendStatus(400)
     }
 
     fs.readFile(dbPath, "utf8", (err, data) => {
         const db = JSON.parse(data)
-        console.log(db);
+        console.log(db)
         db.push(req.body)
         const json = JSON.stringify(db)
-        fs.writeFile(dbPath, json, 'utf8',(err, data)=>{
+        fs.writeFile(dbPath, json, 'utf8', (err, data) => {
             res.send(json)
         })
     });
 
 });
 
-app.get("/", (req, res) => {
-    fs.readFile(dbPath, (err, json) => {
-        let obj = JSON.parse(json);
-        res.json(obj);
+app.delete("/:id", (req, res) => {
+    const { id } = req.params
+
+    fs.readFile(dbPath, "utf8", (err, data) => {
+        const db = JSON.parse(data)
+
+        db = db.filter(el => {
+            return el.id !== id
+        })
+
+        const json = JSON.stringify(db)
+
+        fs.writeFile(dbPath, json, 'utf8', (err, data) => {
+            res.send(json)
+        })
     });
+
 });
 
-app.listen(3000, ()=>console.log(`Server started on port ${3000}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
